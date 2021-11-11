0218e7ad7396d6dc3e7dd9a71c315e7c715ee5c8
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-11 23:08:26 +0100

    style card

--- src/component/CommentArea.jsx
+++ src/component/CommentArea.jsx
@@ -1,5 +1,5 @@
 import {Component} from 'react'
-import {Button} from 'react-bootstrap'
+import {Button, Form} from 'react-bootstrap'
 
 export default class CommentArea extends Component{
 
@@ -9,11 +9,17 @@ state={
 
 render=()=>{
     return (
-    <>
-    <h5 className="mr-3" style={{backgroundColor:'yellow'}}>  CommentArea</h5>
-    <Button variant="secondary" size="sm">Add comment</Button>
-    <Button variant="primary" size="sm">Comment list </Button>
-    </>
+    <div className="mb-3 py-2  text-center" style={{backgroundColor:'yellow'}}>
+    
+    <Button variant="secondary" size="sm" className="mb-2 ml-2">Add comment</Button>
+    <Button variant="primary" size="sm" className="mb-2 ml-2">Comment list </Button>
+    <Form.Select aria-label="Default select example" >
+  <option>Rating</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</Form.Select>
+    </div>
     )
 }
 
--- src/component/singleBook.jsx
+++ src/component/singleBook.jsx
@@ -1,4 +1,4 @@
-import {Card, Col, Form} from 'react-bootstrap'
+import {Card, Col} from 'react-bootstrap'
 import { Component } from 'react'
 import CommentArea from './CommentArea'
 
@@ -42,7 +42,7 @@ this.setState({comments:books})
     render=()=>{
         return(
 <>
-     <Col sx={3}></Col>
+     <Col sx={3} >
             <Card onClick={()=>this.setState({selected:!this.state.selected})} 
             style={{ border : this.state.selected? '3px solid gray' :'none' }}>
                 <Card.Img variant="top" src={this.props.book.img} />
@@ -56,16 +56,7 @@ this.setState({comments:books})
     this.state.selected && <CommentArea />
     }
     
-
-
-    <Form.Select aria-label="Default select example" style={{ display : this.state.selected? 'block' :'none' }}>
-  <option>Rating</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</Form.Select>
-
-
+</Col>
 
 </>
               
