c7835a93bbe124195f3b171c7bf5464ffbafff32
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-11 09:25:54 +0100

    created class on BookList

--- src/component/bookList.jsx
+++ src/component/bookList.jsx
@@ -1,47 +1,72 @@
-import { Container, Row, Card, Col } from 'react-bootstrap'
-//import Form from 'react-bootstrap/Form'
+import { Container, Row, Form, Col } from 'react-bootstrap'
 import SingleBook from '../component/singleBook.jsx'
+import React from 'react'
+
+/* 
+const filterBookList=(arr, query)=>{
+arr.filter((b)=>b.title.include(query))
+.map((e=>{
+return }))}
+ */
 
 
-
-const BookList = ( {books} )=>(
+class Booklist extends React.Component{
+render(){
+    state={
+      let  itemState = ''
+    }
+    return(
+        <>
 <Container>
-<Row>
-    {books.map((e)=>
-    <SingleBook book={e}/>
-       
-    )}
-</Row>
+    <Row>
+<Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
+    </Row>
 
 </Container>
 
-)
-/* const filterBookList=(arr, query)=>{
-let queryBookResult=arr.filter(bookQuery => bookQuery.title.include(query))
-console.log(queryBookResult)
-/* .map(book=>
-    <SingleBook element={book}/>
-       )
-} 
 
-const BookList=()=>(
-<Form >
-  <Form.Group className="mb-3" >
-    <Form.Label>Search a book by name</Form.Label>
-    <Form.Control 
-    type="text" 
-    placeholder="insert a name" 
-    // value="this.state.value"
-    onChange={(e)=>{
-                filterBookList(books, e.target.value)
+
+// display section
+<Container>
+<Row>
+    {
+    this.props.books.map(e=>
+    (<Col xs={6} md={3}>
+       <SingleBook book={e}/> 
+    </Col>))
     }
-    }
-           />
-      </Form.Group>
-</Form>
+    
+</Row>
+
+</Container>
+)
 
     
-)
- */
+}
 
-export default BookList
+}
+
+
+ </>
+
+export default Booklist
