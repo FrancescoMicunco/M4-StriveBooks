9d81f790e8683a8641101056a38f816d40107b28
Francesco Micunco <Francescomicunco@gmail.com>
2022-01-04 13:56:40 +0100

    Update footer.jsx

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -5,7 +5,7 @@ const MyFooter = ()=>(
         <main role="main" class="container">
       <h1 class="mt-5">About Us</h1>
       <p class="lead">Search our nearest store</p>
-      <p>Use <a href="../sticky-footer-navbar/">our mail address to place some specia question</a> </p>
+      <p>Use <a href="../sticky-footer-navbar/">our mail address to place any special question</a> </p>
     </main>
     </Container>
 
