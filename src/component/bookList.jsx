68dc75ae5dfda797819de75433bf048a581041df
Francesco Micunco <Francescomicunco@gmail.com>
2022-01-04 13:56:11 +0100

    Update bookList.jsx

--- src/component/bookList.jsx
+++ src/component/bookList.jsx
@@ -1,33 +1,43 @@
 import { Container, Row, Form, Col } from "react-bootstrap";
 import SingleBook from "../component/singleBook.jsx";
 import { useState } from "react";
-import books from "../db/scifi.json";
 
-const Booklist = (props) => {
-  
 
+const Booklist = ({books}) => {
+  const [Item, setItem] = useState('');
     
   return (
     <>
-      {/* // display section */}
-      <Container>
-        <Row>
-          <Col sx={12} md={9}>
-            <Row>
-              {books
-                .filter((b) => b.title.includes())
-                .map((e) => (
-                  <Col md={4}>
-                    <SingleBook book={e} asin={books.asin} setId={setId} />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-          
-        </Row>
-      </Container>
-    </>
-  );
+<Container>
+    <Row>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Search by Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter title"
+            value={Item}
+            onChange={(e) => {
+            setItem(e.target.value);
+              
+            }}
+          />
+        </Form.Group>
+      </Form>
+    </Row>
+  </Container>
+  <h2>Books Title</h2>
+ 
+  <Container>
+    <Row >{books.filter(b=>b.title.toLowerCase().includes(Item)).map(b=>
+      <Col sx={12} md={3}>
+        <SingleBook book={b}/>
+      </Col>)}
+    </Row>
+  </Container>
+   
+   </>
+  )
 };
 
 export default Booklist;
