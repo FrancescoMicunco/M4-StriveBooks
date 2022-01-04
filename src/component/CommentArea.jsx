927a9d1df492e36327de1effdbd2ead32b047540
Francesco Micunco <Francescomicunco@gmail.com>
2022-01-04 18:41:35 +0100

    Update CommentArea.jsx

--- src/component/CommentArea.jsx
+++ src/component/CommentArea.jsx
@@ -1,28 +1,48 @@
-import {Component} from 'react'
-import {Button, Form} from 'react-bootstrap'
+import {Col, Button} from 'react-bootstrap'
 // import AddComment from './AddComment'
-// import CommentsList from './CommentsList'
+import CommentsList from '../component/CommentsList'
+import { useEffect, useState} from 'react'
+
+const CommentArea = (props)=>{
+console.log("this is props from comment area", props.id )
+  const [comments, setComments]=useState([])
+
+  const getComments = async (id) => {
+    
+    let headers = {
+      Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOGQ1MWFhY2FhMjAwMTU1MmExNzUiLCJpYXQiOjE2NDEyODIzMTYsImV4cCI6MTY0MjQ5MTkxNn0.6iYNJ5P3WYjLBc_-n0eH4yoCDkgwwlk6NTzKL_OmtiA",
+    };
+
+    let res = await fetch(`https://striveschool-api.herokuapp.com/api/comments/`+id, {
+      headers,
+    });
+    try {
+      if (res.ok) {
+        let data = await res.json();
+        console.log("those are data",data.comment)
+       setComments(data)
+      } else {
+        alert("bad request");
+      }
+    } catch (error) {
+      console.log("Something wrong on your connection or server connection");
+    }
+  };
+
+useEffect(()=>{getComments(props.id)}, [])
 
 
-
-export default class CommentArea extends Component{
-
-state={
-
-}
-
-render=()=>{
     return (
-<Col sx={12} md={3}>
-            <h1>Comments</h1>
-            <ul>
-              <CommentsList asin={books.asin} />
+          <Col sx={12} >
+            <h3>Comments</h3>
+            <Button>add</Button>
+            <ul>{
+               <CommentsList comments={comments} />
+              }
             </ul>
           </Col>
-
-    
-    </div>
-    )
+   )
 }
 
-}
+export default CommentArea
