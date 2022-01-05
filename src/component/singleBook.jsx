2502a8601feb19adb8f8fa509da84760cc5e2de6
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-05 16:16:39 +0100

    badge

--- src/component/singleBook.jsx
+++ src/component/singleBook.jsx
@@ -1,6 +1,8 @@
 import { Card, } from "react-bootstrap";
 import React, { useState } from "react";
 import CommentArea from "./CommentArea";
+import MyBadge from './myBadge'
+
 
 const SingleBook = ({book}) => {
 
@@ -9,21 +11,21 @@ const SingleBook = ({book}) => {
  const [asin, setAsin] = useState("");
  
 
-  // ================RENDER AREA
+  // ================  RENDER AREA
 
   return (
     
     <>
-        <Card onClick={()=> {setSelected(!selected); setAsin(book.asin)}}        
-style={{border: selected ? "2px solid gray" : "none"}}
-
-       >
-          <Card.Img variant="top" src={book.img} />
-          <Card.Body>
-            <Card.Title>{book.title}</Card.Title>
-           </Card.Body>
+            <Card   onClick={()=> {setSelected(!selected); setAsin(book.asin)}}        
+                    style={{border: selected ? "2px solid gray" : "none"}}>
+                <MyBadge text={"-20%"} style={{position:"absolute", top:"0", let:"0"}}/>
+            <Card.Img variant="top" src={book.img} />
+              <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+              </Card.Body>
         </Card>
-        <div style={{display: selected ? "block" : "none"}}>
+         
+                <div style={{display: selected ? "block" : "none"}}>
         
         <CommentArea asin={asin}/>
       </div>
