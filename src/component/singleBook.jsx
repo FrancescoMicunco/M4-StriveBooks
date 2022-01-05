f0e0cc3799fc55b8cdcfa13e413879ce0d7ffd48
Francesco Micunco <Francescomicunco@gmail.com>
2022-01-05 15:07:04 +0100

    Update singleBook.jsx

--- src/component/singleBook.jsx
+++ src/component/singleBook.jsx
@@ -3,32 +3,29 @@ import React, { useState } from "react";
 import CommentArea from "./CommentArea";
 
 const SingleBook = ({book}) => {
- 
+
+
  const [selected, setSelected] = useState(false);
-  const [border, setborder] = useState("");
-  const [isActive, setisActive] = useState(false);
-  const [id, setId] = useState("");
-  const [comments, setcomments] = useState([]);
+ const [asin, setAsin] = useState("");
+ 
 
   // ================RENDER AREA
 
   return (
     
     <>
-        <Card onClick={()=> {setSelected(!selected); setId(book.asin)}}        
+        <Card onClick={()=> {setSelected(!selected); setAsin(book.asin)}}        
 style={{border: selected ? "2px solid gray" : "none"}}
 
        >
           <Card.Img variant="top" src={book.img} />
           <Card.Body>
             <Card.Title>{book.title}</Card.Title>
-
-            
-            </Card.Body>
+           </Card.Body>
         </Card>
         <div style={{display: selected ? "block" : "none"}}>
         
-        <CommentArea id={id}/>
+        <CommentArea asin={asin}/>
       </div>
     </>
   );
